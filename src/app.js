98e3891bcf60fcf6b70e7ea3caef1baa468a92a2
kmgaikwad97 <kmgaikwad97@gmail.com>
2024-02-29 00:49:39 +0530

    chnages

--- src/app.js
+++ src/app.js
@@ -34,7 +34,6 @@ hbs.registerPartials(partials_path);
 
 // env 
 console.log(process.env.SECRET_KEY);
-// asasasasasasasasasasasasasasasasas
 
 
 
@@ -55,9 +54,6 @@ app.get("/login", (req, res) => {
 // create a new user in our database
 app.post("/register", async (req, res) => {
     try {
-        // console.log(req.body.firstname);
-        // res.send(req.body.firstname)
-
         const password = req.body.password;
         const cpassword = req.body.confirmpassword;
 
@@ -72,33 +68,12 @@ app.post("/register", async (req, res) => {
                 password: password,
                 confirmpassword: cpassword
             })
-
-
             console.log("the success part" + registerEmployee);
-
-
-
             const token = await registerEmployee.generateAuthToken();
             console.log("the token part" + token);
-
-
-
-
-
-
-
             //******* after the next *******// 
-
-
-
-
-
-
-
             const registered = await registerEmployee.save();
-
             console.log("the page part"+registered);
-
             res.status(201).render("index");
 
         } else {
@@ -109,107 +84,26 @@ app.post("/register", async (req, res) => {
     }
 })
 
-
-
-
-
-
 // validation for login
 app.post("/login", async (req, res) => {
     try {
         const email = req.body.email;
         const password = req.body.password;
-
         // console.log(`${email} is emaiil and password is ${password}`);
-
         const useremail = await Register.findOne({ email: email }) //object destruction below // Register.findOne({email});
-
-
-
-
-
         const isMatch = await bcrypt.compare(password, useremail.password);
-
-
-
         const token = await useremail.generateAuthToken();
             console.log("the token part" + token);
-
-
-
         if (isMatch) {
             res.status(201).render('index');
         }
         else {
             res.send('invalid login details')
         }
-
     } catch (error) {
         res.status(400).send('invalid login details')
     }
 })
-
-
-
-
-// const bcrypt = require('bcryptjs');
-
-
-
-
-// const securePassword = async(password)=>{
-
-//     const passwordHash = await bcrypt.hash(password, 10);
-//     console.log(passwordHash);
-//     // $2a$10$QRjsDgtDDRRhqRgupeTvg.NejKX3vKjnYUQIUlx4DBbIa9gficuEG
-
-//     const passwordMatch = await bcrypt.compare(password, passwordHash);
-//     console.log(passwordMatch);
-//     // true
-// }
-// securePassword("kshitij@123");
-
-
-
-
-
-
-// // ************ JWT ********************
-
-// const jwt = require("jsonwebtoken");
-// const async = require("hbs/lib/async");
-
-// const createToken = async ()=>{
-//     // jwt.sign({_id},)
-//     // jwt.sign({_id:"622c7e2e996732e696f8ff64"},"secretKey",)
-//    const token = await jwt.sign({_id:"622c7e2e996732e696f8ff64"},"djkbfkfgjejweerjejwerererjdjdjds",
-//    {expiresIn:"5 secs"})
-
-// //    { _id: '622c7e2e996732e696f8ff64', iat: 1647436571, exp: 1647436576 }
-
-//     console.log(token);
-
-//     // generated token
-//     // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjJjN2UyZTk5NjczMmU2OTZmOGZmNjQiLCJpYXQiOjE2NDc0MzYxMDB9.AI3ljAUMqatWn_b2ZggADlgddDkWAp51fbVd6J8Y6CI
-
-
-
-//     const userVar = await jwt.verify(token,"djkbfkfgjejweerjejwerererjdjdjds")
-//     console.log(userVar);
-
-//     // { _id: '622c7e2e996732e696f8ff64', iat: 1647436100 }
-// }
-
-// createToken();
-
-
-
-
-
-
-
-
-
 app.listen(port, () => {
     console.log(`server connected at ${port}`);
 })
---
+++ src/models/readme.md
@@ -0,0 +1,81 @@
+# Angular-basic-CRUD-
+Basic CRUD Operation using **Angular 13**
+A simple Angular application demonstrating **CRUD** (**Create, Read, Update, Delete**) operations.
+
+## Overview
+This project aims to illustrate a basic CRUD application built with Angular 13. It enables users to perform essential operations on user data, such as adding, updating, and deleting user information.
+
+## Features
+>Create: Add new users with their name, mobile number, email, and password.
+
+>Read: Display a table showing all existing users with their details.
+
+>Update: Edit and update user information.
+
+>Delete: Remove users from the list.
+
+## Getting Started
+### Prerequisites
+Node.js installed on your machine(**v16**).  
+The default color is `#ffffff` & `#0000000`
+Angular CLI(**v13.0.4**)
+Installation
+Clone this repository.
+
+bash/Terminal
+
+Copy code   
+`git clone https://github.com/kmgaikwad97/Angular-basic-CRUD-.git`  
+Navigate into the project directory.
+
+Copy code   
+`cd your-repo-name`   
+Install dependencies.
+ 
+Copy code   
+`npm install` / `yarn`   
+Run the application.
+
+Copy code   
+`ng serve`
+
+## Usage   
+Access the application in your browser at http://localhost:4200.   
+Fill in the form to add a new user.   
+View, edit, or delete existing users from the table.
+
+
+## Tech Stack
+>**Angular 13**   
+>**HTML/CSS**   
+>**TypeScript**   
+
+
+## Contributing   
+Contributions are welcome! Feel free to open issues or submit pull requests. Suggestions are welcomed.
+
+
+## Contributing
+🎉 Contributions and Suggestions Welcome!
+
+If you have any ideas to improve this project, encountered issues, or wish to contribute enhancements, please consider the following options:
+
+## Opening Issues
+Found a bug or have a feature request? Please open an issue to describe the problem or propose your idea. Be sure to provide relevant details, such as steps to reproduce any bugs.
+
+## Pull Requests
+We'd love your help in enhancing this project! If you've made improvements and wish to share them:
+
+>Fork the repository and `create your branch (git checkout -b my-amazing-feature)`.   
+   
+>Commit your changes with descriptive messages `(git commit -am 'Added a new feature')`.   
+
+>Push to the branch `(git push origin my-amazing-feature)`.   
+
+>Open a pull request detailing the changes made and any relevant information.   
+
+## Suggestions and Feedback
+
+Do you have any suggestions, feedback, or questions about the project? Feel free to reach out or open a discussion in the repository. Your input is highly valued and helps in making this project better for everyone.
+
+Your involvement in improving this project is greatly appreciated!
